7ee34f80beb03a14e4451a3b011447996c473661
federicotomas79 <federicotomas79@users.noreply.github.com>
2021-09-24 15:25:41 +1200

    Add files via upload

---
+++ climate stations NOAA.R
@@ -0,0 +1,25 @@
+#Climate stations NOAA list
+#(https://www.ncei.noaa.gov/products/land-based-station/global-historical-climatology-network-daily)
+
+
+if (!require('tidyverse')) install.packages('tidyverse'); library('tidyverse')
+if (!require('readr')) install.packages('foreign'); library('foreign')
+
+ghcnd_stations_names <- read_table("https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt", col_names = FALSE)
+
+ghcnd_stations_names <- plyr::rename(ghcnd_stations_names, c("X1" = "WScode", "X2" = "Lat", "X3"="Long", "X4" = "Alt"))
+ghcnd_stations_names2 <- select(ghcnd_stations_names, -c(8,9)) 
+
+glimpse(ghcnd_stations_names2)
+View(ghcnd_stations_names2)
+
+
+ghcnd_stations_spec <- read_table("https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt", col_names = FALSE)
+ghcnd_stations_spec  <- plyr::rename(ghcnd_stations_spec, c("X1" = "WScode", "X2" = "Lat", "X3"="Long", "X4" = "Element_rec", 
+                                                            "X5" = "Begin_date", "X6" = "End_date"))
+
+glimpse(ghcnd_stations_spec)
+View(ghcnd_stations_spec)
+
+
+
