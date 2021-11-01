892a8647347951f89b8f8a01be09a4a4fab82daf
Craig Phillips <40811133+craigbp@users.noreply.github.com>
2021-11-01 18:58:54 +1300

    update "locations_of_stations_used_by_worldclim.r"
    To "locations_of_stations_used_by_worldclim.r", I added explanatory comments & some code for leaflet-plotting the worldclim station locations. Also committed the leflet plot itself, which is the one I sent to MPI on 20211005.

--- locations_of_stations_used_by_worldclim.r
+++ locations_of_stations_used_by_worldclim.r
@@ -1,4 +1,7 @@
-# 20211005 This script doesn't actually get or provide access to climate data. Rather it shows the locations of climate stations that contributed data to WorldClim (WC). MPI has specified that--for the pairwise location vs location climate match part of the work--it wants us to use actual climate station data rather than WC data. I think this is because WC data are gridded, thus data for some WC locations without climate stations must be spatially interpolated (& perhaps inaccurate). However, we would avoid any spatial interpolation if we used the subset of WC data that came directly from climate station locations. This would have 2 main benefits: 1) The same WC data could be used in both the climate station vs climate station pairwise comparisons & the NZ vs world comparisions; 2) The WC data are already nicely formatted for our needs (unlike raw NOAA data). I will ask MPI about this idea.
+# Craig Phillips 20211005: This script doesn't actually get or provide access to climate data. Rather it shows the locations of climate stations that contributed data to WorldClim (WC). MPI has specified that--for the pairwise location vs location climate match part of the work--it wants us to use actual climate station data rather than WC data. I think this is because WC data are gridded, thus data for some WC locations without climate stations must be spatially interpolated (& perhaps inaccurate). However, we would avoid any spatial interpolation if we used the subset of WC data that came directly from climate station locations. This would have 2 main benefits: 1) The same WC data could be used in both the climate station vs climate station pairwise comparisons & the NZ vs world comparisions; 2) The WC data are already nicely formatted for our needs (unlike raw NOAA data). I will ask MPI about this idea.
+# Craig Phillips 20211101: Discussed using WC data from climate station locations with MPI on 20211006 & they agreed. On the same day I emailed a leaflet map of WC climate station locations (produced by this script) to Stephan Halloy & Ursula Torres (cc, FT & JK) & asked them to choose the locations they would like to use. (I also pushed the leaflet map to this current "Worldclim-stations" repo to.) SH & UT agreed, but none sent to us, so at our next meeting I asked if they could at least choose a few for us to start developing a workflow with: However, as of today still no response.
+# We should go ahead & choose 10 or so stations ourselves to start developing the workflow. Today I asked FT to choose 10 locations then write a script for extracting their climate variables from WorldClim rasters. Suggested as a starting point for that script he could copy "crop_worldclim_monthly_to_nz_and_save_dataframe.r" from my "world_clim" GitHub repo. 
+# After we have the data, we can calculate CMIs between each pair. It might be good to do this with our R version of MCR because it won't be computationally intensive, & using the R version will get me started on refining it for NCSU.
 
 rm(list = ls())
 library(tidyverse)
@@ -21,6 +24,7 @@ sum(cntry$count) # 55879
 
 #------------------------------------------------------------------
 # Extract only climate stations data with Precipitation, Tmin & Tmax
+#------------------------------------------------------------------
 any(is.na(wc$RAINID)) # check if contains both NA & "NA"
 any(is.na(wc$TMINID)) 
 any(is.na(wc$TMAXID)) 
@@ -37,7 +41,6 @@ glimpse(vars3)
 
 sum(vars3$with3vars) # 10531
 
-#------------------------------------------------------------------
 # Look at summarised data
 summ <- left_join(cntry, vars3, by = 'COUNTRY')
 
@@ -48,8 +51,8 @@ summ %>%
   DT::datatable(caption = mycap, options = list(pageLength = 20))
 
 #------------------------------------------------------------------
-# Plot locations
-
+# Static plot of locations
+#------------------------------------------------------------------
 w <- map_data('world')
 
 ggplot() +
@@ -61,6 +64,46 @@ ggplot() +
 	theme_bw() +
 	coord_quickmap()
 
+#------------------------------------------------------------------
+# Leaflet plot of locations with TMIN, TMAX & RAIN
+#------------------------------------------------------------------
+wc2 <- wc %>% 
+	filter(RAINID != "NA" & TMINID != "NA" & TMAXID != "NA") %>%
+	select(-c(RAINID: TMEANID))
 
+length(which(is.na(wc2$COUNTRY)))	
+length(unique(wc2$NAME)) # 10105
+# tail(sort(unique(wc2$NAME)), n = 100)
 
+glimpse(wc2)
+  
+library(leaflet)
+
+map <-
+	leaflet(data = wc2, options = leafletOptions(preferCanvas = TRUE)) %>% 
+		addProviderTiles(providers$Esri.NatGeoWorldMap,
+			options = providerTileOptions(
+				updateWhenZooming = FALSE, # map won't update tiles until zoom is done
+				updateWhenIdle = TRUE)) %>% # map won't load new tiles when panning
+			leaflet.extras::addSearchOSM() %>% # search for names within tiles
+			leaflet.extras::addResetMapButton() %>% # go back to default zoom
+			addControl('Hover for climate station ID, click for ID, coordinates & name (AGR 20211006)', position  =  'topright') %>%
+			setView(0, 0, zoom = 2) %>% # initialise view extent
+			setMaxBounds(lng1 = -200,
+									 lat1 = 100,
+									 lng2 = 200,
+									 lat2 = -100) %>%
+    clearMarkers() %>%
+    addCircleMarkers(lng = wc2$LONG, lat = wc2$LAT, weight = 1.5,
+			color = "#FFFFFF",
+			opacity = 1,
+			fillColor = "#000000",
+			fillOpacity = 1,
+			radius = 4,
+      label = ~as.character(WCID), 
+      popup = ~as.character(paste0('Id=', WCID, '; Lon=', LONG, '; Lat=', LAT, '; Name=', NAME)))
+
+# map
+
+htmlwidgets::saveWidget(widget = map, file = 'climate_stations_with_tmin_tmax_prec_used_by_wordclim.html', selfcontained = TRUE)
 
